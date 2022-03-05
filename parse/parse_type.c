c4829143243bc03476183a8f297319fc1a753c98
gynhyunkim <gyeonghyun.kimm@gmail.com>
2022-03-05 13:21:58 +0900

    update: xpm 파일인자 오류 처리

--- parse/parse_type.c
+++ parse/parse_type.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parse_type.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: minchoi <minchoi@student.42seoul.kr>       +#+  +:+       +#+        */
+/*   By: gkim <gkim@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/24 17:06:57 by minchoi           #+#    #+#             */
-/*   Updated: 2022/02/26 20:12:20 by minchoi          ###   ########.fr       */
+/*   Updated: 2022/03/05 13:20:48 by gkim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,14 +56,16 @@ int	parse_xpm(char **split, t_data *data)
 		ft_free(split);
 		return (print_err(INVALID_ELEMENT));
 	}
-	if (!ft_strcmp(split[0], "NO"))
+	if (!ft_strcmp(split[0], "NO") && !data->xpm_path[NO])
 		data->xpm_path[NO] = split[1];
-	else if (!ft_strcmp(split[0], "SO"))
+	else if (!ft_strcmp(split[0], "SO") && !data->xpm_path[SO])
 		data->xpm_path[SO] = split[1];
-	else if (!ft_strcmp(split[0], "WE"))
+	else if (!ft_strcmp(split[0], "WE") && !data->xpm_path[WE])
 		data->xpm_path[WE] = split[1];
-	else if (!ft_strcmp(split[0], "EA"))
+	else if (!ft_strcmp(split[0], "EA") && !data->xpm_path[EA])
 		data->xpm_path[EA] = split[1];
+	else
+		return (print_err(INVALID_ELEMENT));
 	split[1] = NULL;
 	data->flag++;
 	ft_free(split);
@@ -85,8 +87,10 @@ int	parse_color(char **split, t_data *data)
 	}
 	if (!ft_strcmp(split[0], "F"))
 		put_floor(split, data);
-	else
+	else if (!ft_strcmp(split[0], "C"))
 		put_ceiling(split, data);
+	else
+		return (print_err(INVALID_ELEMENT));
 	data->flag++;
 	ft_free(split);
 	return (0);
